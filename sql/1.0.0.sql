e76a8999f7fd1761c3b4e6e4fb539d9c77d615cf
Luis <Luis@192.168.0.116>
2013-02-23 16:36:35 -0300

    correção para include

---
+++ sql/1.0.0.sql
@@ -0,0 +1,16 @@
+﻿create table grupo(
+	id serial primary key,
+	nome  character varying(200),
+	data_criacao timestamp
+);
+
+create table usuarios(
+	id serial primary key,
+	nome character varying(200),
+	email character varying(200),
+	senha character varying(200),
+	data_criacao timestamp,
+	grupo_id integer,
+	constraint fk_usu_grupo foreign key (grupo_id)
+		references grupo(id)
+);
