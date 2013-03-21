73724d070aa6b5794cc9e345eaf9dc0b251f005f
Luis Carlos Boch <luis.c.boch@gmail.com>
2013-03-20 22:30:40 -0300

    carregar resultado da pesquisa usando o ajax.

--- application/views/resultadoPesquisa.php
+++ application/views/resultadoPesquisa.php
@@ -1,15 +1,14 @@
 <?
-if(isset($resultado) && is_array($resultado)){
-    
-foreach($resultado as $v){
-  ?>
-<div class="resultado-pesquisa">
-    <div class="pesquisa-titulo"><a href="<?=site_url($v->getLink())?>"><?=$v->getTitulo()?></a></div>
-    <div class="pesquisa-desc"><a href="<?=site_url($v->getLink())?>"><?=$v->getBreveDescricao()?></a></div>
-</div>
-      
-  <?
-}
+if (isset($resultado) && is_array($resultado)) {
 
+    foreach ($resultado as $v) {
+        ?>
+        <div class="resultado-pesquisa">
+            <div class="pesquisa-titulo"><a href="<?= site_url($v->getLink()) ?>" onclick="return carregar('<?=$v->getLink()?>');"><?= $v->getTitulo() ?></a></div>
+            <div class="pesquisa-desc"><a href="<?= site_url($v->getLink()) ?>" onclick="return carregar('<?=$v->getLink()?>');"><?= $v->getBreveDescricao() ?></a></div>
+        </div>
+
+        <?
+    }
 }
 ?>
