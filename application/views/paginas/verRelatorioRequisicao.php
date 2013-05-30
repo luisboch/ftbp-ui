26b2ec21f859c10be6d9a968316879c5cdab3099
felipe <felipe@vmxp.home>
2013-05-30 12:13:47 -0300

    inicio criacao relatorio requisicao

--- application/views/paginas/verRelatorioRequisicao.php
+++ application/views/paginas/verRelatorioRequisicao.php
@@ -6,7 +6,17 @@
         </tr>
         <tr>
             <td>Nome</td>
+            <td>Departamento</td>
             <td>Qtde</td>
         </tr>
     </thead>
+    <tbody>
+        <?foreach ($reqst as $v){?>
+        <tr>
+            <td><?= $v->getUsuario()->getNome()?></td>
+            <td><?= $v->getDepartamento()->getNome()?></td>
+            <td><?= $v->getQtde()?></td>
+        </tr>
+        <?}?>
+    </tbody>
 </table>
