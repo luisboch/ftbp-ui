d9e15768e862e3d931a179c115ad5011dfe69dfe
Luis Boch <luis.c.boch@gmail.com>
2013-06-02 10:49:14 -0300

    iniciado relatório de cursos

---
+++ application/controllers/RelatorioCursoController.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once 'ftbp-src/servicos/impl/ServicoRelatorioRequisicao.php';
+
+require_once 'ftbp-src/entidades/basico/RelatorioRequisicao.php';
+
+/**
+ * Description of AvisoController
+ *
+ * @author felipe
+ */
+class RelatorioRequisicaoController extends MY_Controller {
+
+    /**
+     *
+     * @var ServicoRelatorioRequisicao
+     */
+    private $servico;
+
+    function __construct() {
+        parent::__construct();
+        $this->servico = new ServicoRelatorioRequisicao();
+    }
+
+    public function index() {
+
+        $this->view('paginas/relatorioRequisicao');
+    }
+
+    public function gerarRelatorio() {
+        // Recupera o id que veio do form.
+        $r = new RelatorioRequisicao();
+        $r->setTipo($_POST['tipo']);
+        $r->setDataInicio($_POST['dataInicio']);
+        $r->setDataFim($_POST['dataFim']);
+
+        $n = new RelatorioRequisicao();
+        try {
+
+            if ($r->getTipo() == 1) {
+                $n = $this->servico->gerarRelatorioFechamento($r);
+                $tipo = 'Fechamento';
+            } else {
+                $n = $this->servico->gerarRelatorioAbertura($r);
+                $tipo = 'Abertura';
+            }
+
+            $this->view('paginas/verRelatorioRequisicao.php', array('reqst' => $n, 'r' => $r, 'titulo' => $tipo));
+        } catch (ValidacaoExecao $e) {
+
+            // Se não encontrar exibe 404
+            foreach ($e->getErrors() as $v) {
+                $this->warn($v->getMensagem(), $v->getCampo());
+            }
+
+            exit;
+        }
+    }
+
+    public function gerarPdf() {
+
+        $tipo = $_POST['tipo'];
+        $rq = new RelatorioRequisicao();
+        $rq->setTipo($_POST['tipo']);
+        $rq->setDataInicio($_POST['dataInicio']);
+        $rq->setDataFim($_POST['dataFim']);
+
+        if ($rq->getTipo() == '1') {
+            $n = $this->servico->gerarRelatorioFechamento($rq);
+        } else {
+            $n = $this->servico->gerarRelatorioAbertura($rq);
+        }
+
+        $this->load->library('pdf'); // Load library
+        // Generate PDF with FPDF
+        
+        $header = array('Nome', 'Departamento', 'Quantidade');
+        
+        $data = array();
+        
+        
+        foreach ($n as $r) {
+            $data[] = array($r->getUsuario()->getNome(), $r->getDepartamento()->getNome(), $r->getQtde());
+        }
+        
+        $this->pdf->SetTitle('Relatório de requisições');
+        
+        $this->pdf->AddPage();
+        
+        $this->pdf->setColumnSize(array(80, 80, 30));
+        
+        $this->pdf->FancyTable($header, $data);
+        
+        $this->pdf->Output('relatorio_requisicao_'.date('d-m-y_H-i').'.pdf', 'D');
+    }
+
+}
+
+?>
---
+++ application/views/paginas/relatorioCurso.php
@@ -0,0 +1,40 @@
+<? /* @var $_grupo Grupo */ ?>
+<script type="text/javascript">
+    $(function() {
+        $('.data').datepicker({dateFormat: "yy-mm-dd"});
+    })
+</script>
+
+<form method="post" id="form-cadastro" action="<?= site_url('RelatorioRequisicaoController/gerarRelatorio') ?>" >
+
+    <table class="form-table">
+        <caption><span>Relatório de Requisições</span></caption>
+        <tbody>
+            <tr>
+                <td>Selecione um tipo</td>
+                <td>
+                    <select id="tipo" name="tipo">
+                        <option></option>
+                        <option value="0">Abertura</option>
+                        <option value="1">Fechamento</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Periodo</td>            
+                <td>
+                    <input type="text" class="data" id="dataInicio" name="dataInicio" placeholder="Data de Inicio">
+                    <input type="text" class="data" id="dataFim" name="dataFim" placeholder="Data Final">
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <? if ($_grupo != null && $_grupo->temAcesso(GrupoAcesso::REQUISICAO, true)) { ?>
+                        <input type="submit" value="Enviar">
+                    <? } ?>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</form>    
