6569452bc58347af2fdc614cfe71d2096f25bb44
luis <luis@localhost>
2013-10-05 18:24:01 -0300

    update no banco em relação ao ajuste aplicado aos cursos.

---
+++ sql/1.0.1.sql.sql
@@ -0,0 +1,14 @@
+-- Drop old columns
+alter table curso drop column coordenador; 
+alter table curso drop column email;
+alter table curso drop column credito;
+alter table curso drop column contatosecretaria;
+
+-- Create new columns
+alter table curso add column coordenador_id integer;
+alter table curso add column contato_id integer;
+alter table curso add constraint fk_contato foreign key (contato_id)
+	references usuarios(id);
+alter table curso add constraint fk_coordenador foreign key (coordenador_id)
+	references usuarios(id);
+
